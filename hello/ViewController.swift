740bb2d53373ca673737b73004dc295312dfb60b
SeungHee Seo <https://github.com/blingsh>
2022-01-28 21:45:28 +0900

    9장hello실습완료

--- hello/ViewController.swift
+++ hello/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblHello1: UILabel!
+    //@IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello1.text = "Hello, " + txtName.text!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
