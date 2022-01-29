567698eede933a3907779e4e029d300ed6698670
SeungHee Seo <https://github.com/blingsh>
2022-01-29 14:41:43 +0900

    10장스토리보드안쓰고label만들기

--- hello/ViewController.swift
+++ hello/ViewController.swift
@@ -19,6 +19,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let label = UILabel()
+        label.frame = CGRect(x:10, y:100, width: 100, height: 50)
+        label.text = "Hello"
+        label.textColor = UIColor.red
+        label.backgroundColor = .blue
+        view.addSubview(label)
     }
 
 
